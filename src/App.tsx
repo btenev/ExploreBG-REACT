73445d80b9de520837ae821cb539196bd8a07736
btenev <boyan.tenev@gmail.com>
2025-03-24 21:32:13 +0200

    Build Logo component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Logo from './components/Logo/Logo';
+
 function App() {
   return (
     <>
       <h1>Hi ExploreBg!</h1>
       <p>Some text</p>
+      <Logo />
     </>
   );
 }
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,20 @@
+import './Logo.scss';
+import logo from '../../assets/images/logo.png';
+
+const Logo = () => {
+  return (
+    <figure className="logo">
+      <figcaption>Explore BG</figcaption>
+      <img
+        src={logo}
+        width={40}
+        height={40}
+        loading="eager"
+        alt="Logo"
+        title="Logo"
+      />
+    </figure>
+  );
+};
+
+export default Logo;
