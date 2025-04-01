5034eaa602da8b2ea6ccf8672b15033fce2ef81d
btenev <boyan.tenev@gmail.com>
2025-04-01 12:44:15 +0300

    Install and set up React Toastify.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import './global-styles/main.scss';
 import router from './routes.tsx';
@@ -15,6 +17,17 @@ createRoot(document.getElementById('root')!).render(
     <ProvideTheme>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ProvideTheme>
