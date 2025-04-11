27cb17286d6306fae1ba96e69cd34e66efe80938
btenev <boyan.tenev@gmail.com>
2025-04-11 14:50:16 +0300

    Add custom hook to close element on outside click or Escape/Tab key press.

---
+++ src/hooks/uiHooks/index.ts
@@ -0,0 +1,1 @@
+export { default as useCloseOnEscapeTabClick } from './useCloseOnEscapeTabClick';
---
+++ src/hooks/uiHooks/useCloseOnEscapeTabClick.ts
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction, RefObject, useEffect } from 'react';
+
+type OnCloseType = Dispatch<SetStateAction<MouseEvent | KeyboardEvent>>;
+
+const useCloseOnEscapeTabAndClickOutside = (ref: RefObject<HTMLElement>, onClose: OnCloseType) => {
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        onClose(event);
+      }
+    };
+
+    const handleEscapeAndTabKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape' || event.key === 'Tab') {
+        onClose(event);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+    document.addEventListener('keydown', handleEscapeAndTabKey);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+      document.removeEventListener('keydown', handleEscapeAndTabKey);
+    };
+  }, [ref, onClose]);
+};
+
+export default useCloseOnEscapeTabAndClickOutside;
